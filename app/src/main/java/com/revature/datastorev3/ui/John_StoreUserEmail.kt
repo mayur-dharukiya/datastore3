6bae3d2951a921753755b60895c7e2fa017b71bb
hlljh5 <john158@revature.net>
2022-03-21 16:57:15 -0500

    Updates made

--- app/src/main/java/com/revature/datastorev3/ui/John_StoreUserEmail.kt
+++ app/src/main/java/com/revature/datastorev3/ui/John_StoreUserEmail.kt
@@ -1,11 +1,39 @@
 package com.revature.datastorev3.ui
 
-class John_StoreUserEmail {
+import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+
+
+class John_StoreUserEmail (private val context:Context){
 
     companion object {
 
-        private val Context.dataStore
+        private val Context.dataStoree: DataStore<Preferences> by preferencesDataStore("userDataFile")
+        val USER_EMAIL_KEY = stringPreferencesKey("user_email")
     }
 
+    //get email value from datastore
 
+    val getEmail: Flow<String?> = context.dataStoree.data
+        .map { preferences->
+
+            preferences[Mayur_StoreUserEmail.USER_EMAIL_KEY]?:"FIRSTLAST@GMAIL.COM"
+        }
+
+    //save email
+
+    suspend fun saveEmail(name:String)
+    {
+        context.dataStoree.edit { preferences->
+
+            preferences[Mayur_StoreUserEmail.USER_EMAIL_KEY]=name
+
+        }
+    }
 }
