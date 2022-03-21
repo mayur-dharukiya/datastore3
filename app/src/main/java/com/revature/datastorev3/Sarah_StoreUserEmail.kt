226b5a6c57e3d209a171be39457be5d411751909
sarahkorrin <sarahkorrin@tutanota.com>
2022-03-21 16:49:02 -0400

    store user email update

--- app/src/main/java/com/revature/datastorev3/Sarah_StoreUserEmail.kt
+++ app/src/main/java/com/revature/datastorev3/Sarah_StoreUserEmail.kt
@@ -2,15 +2,28 @@ package com.revature.datastorev3
 
 import android.content.Context
 import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import java.util.prefs.Preferences
 
-class Sarah_StoreUserEmail
+class Sarah_StoreUserEmail(private val context: Context)
 {
     companion object // Creating a companion object to make sure that we only have one instance of this class
     {
-        private val Context.dataStore: DataStore<androidx.datastore.preferences.core.Preferences> by preferencesDataStore("userEmail") // datastore file name
+        private val Context.myDataStoreObject: DataStore<androidx.datastore.preferences.core.Preferences> by preferencesDataStore("userEmail") // datastore file name
         val USER_EMAIL_KEY = stringPreferencesKey("user_email") // Key name to retrieve the data
+    }
+
+    // Get email value from datastore
+    val getEmail: Flow<String?> = context.myDataStoreObject.data
+        .map {preferences -> preferences[USER_EMAIL_KEY]?:"FIRSTLAST@GMAIL.COM"}
+
+    // Save email
+    suspend fun saveEmail(name: String)
+    {
+        context.myDataStoreObject.edit{preferences -> preferences[USER_EMAIL_KEY] = name}
     }
 }
