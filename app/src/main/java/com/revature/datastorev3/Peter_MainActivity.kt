ec5b17233becb34d957ba8746d547663c25d468d
linpeterk <100219056+linpeterk@users.noreply.github.com>
2022-03-21 12:57:49 -0700

    updating peter mainactivity

--- app/src/main/java/com/revature/datastorev3/Peter_MainActivity.kt
+++ app/src/main/java/com/revature/datastorev3/Peter_MainActivity.kt
@@ -22,22 +22,9 @@ class Peter_MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Datastore3Theme {
-        Greeting("Android")
     }
 }
