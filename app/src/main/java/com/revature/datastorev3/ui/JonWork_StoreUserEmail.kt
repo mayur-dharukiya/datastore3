2db426ef6553b1c642f0f70ad8c0a54b6e1b448e
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-03-21 13:58:23 -0700

    personal store user file update

--- app/src/main/java/com/revature/datastorev3/ui/JonWork_StoreUserEmail.kt
+++ app/src/main/java/com/revature/datastorev3/ui/JonWork_StoreUserEmail.kt
@@ -1,9 +1,27 @@
 package com.revature.datastorev3.ui
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 
-class JonWork_StoreUserEmail {
+class JonWork_StoreUserEmail(private val context: Context) {
     companion object{
-        private val Context.dataStore
+        private val Context.dataStoree:DataStore<Preferences> by preferencesDataStore("Email")
+        val USER_EMAIL_KEY= stringPreferencesKey("user_email")
     }
+    val getEmail: Flow<String> =context.dataStoree.data.map { preferences->
+        preferences[USER_EMAIL_KEY]?:"FIRSTLAST@gmail.com"
+    }
+    suspend fun saveEmail(name:String)
+    {
+        context.dataStoree.edit { preferences->
+            preferences[USER_EMAIL_KEY]=name
+        }
+    }
+
 }
