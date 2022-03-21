a94a982bb09b67cfcd0a2c55297cc10446bb6c9b
18xxperson <46502267+18xxperson@users.noreply.github.com>
2022-03-21 14:09:29 -0700

    personal store user file update

--- app/src/main/java/com/revature/datastorev3/JonWork_MainActivity.kt
+++ app/src/main/java/com/revature/datastorev3/JonWork_MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.revature.datastorev3.ui.JonWork_StoreUserEmail
 import com.revature.datastorev3.ui.theme.Datastore3Theme
 
 class JonWork_MainActivity : ComponentActivity() {
@@ -27,4 +30,11 @@ class JonWork_MainActivity : ComponentActivity() {
             }
         }
     }
+}
+@Composable
+fun LoginScreen_JonathanWork()
+{
+    val context= LocalContext.current
+    val scope= rememberCoroutineScope()
+    val dataStore=JonWork_StoreUserEmail(context)
 }
