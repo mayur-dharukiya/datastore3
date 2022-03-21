fa037f0b3fa55284ef1a660bad2411df151261f4
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-03-21 16:31:00 -0400

    Kevin's 2nd commit

---
+++ app/src/main/java/com/revature/datastorev3/ui/Kevin_StoreUserEmail.java
@@ -0,0 +1,11 @@
+package com.revature.datastorev3.ui;
+
+import android.content.Context
+
+public class Kevin_StoreUserEmail {
+
+    companion object {
+
+        private val Context.dataStore
+    }
+}
