62377c2832d2b3a85f04e9cca163704e71fc35d0
Kevin-Weakley <kevin.weakley3@gmail.com>
2022-03-21 17:24:20 -0400

    Kevin's 2nd commit

--- app/src/main/java/com/revature/datastorev3/Kevin_MainActivity.kt
+++ app/src/main/java/com/revature/datastorev3/Kevin_MainActivity.kt
@@ -8,8 +8,11 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import com.revature.datastorev3.ui.Kevin_StoreUserEmail
 import com.revature.datastorev3.ui.theme.Datastore3Theme
 
 class Kevin_MainActivity : ComponentActivity() {
@@ -22,9 +25,21 @@ class Kevin_MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-
+                    LoginScreen_Kevin()
                 }
             }
         }
     }
+}
+@Composable
+fun LoginScreen_Kevin()
+{
+    val context= LocalContext.current
+
+    val scope= rememberCoroutineScope()
+
+    val dataStore=Kevin_StoreUserEmail(context)
+
+
+
 }
--- app/src/main/java/com/revature/datastorev3/ui/Kevin_StoreUserEmail.java
+++ app/src/main/java/com/revature/datastorev3/ui/Kevin_StoreUserEmail.java
@@ -1,11 +1,36 @@
 package com.revature.datastorev3.ui;
 
 import android.content.Context
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.edit
+import androidx.datastore.preferences.core.stringPreferencesKey
+import androidx.datastore.preferences.preferencesDataStore
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
+import java.util.prefs.Preferences
 
-public class Kevin_StoreUserEmail {
+public class Kevin_StoreUserEmail(private val context:Context) {
 
     companion object {
 
-        private val Context.dataStore
+        private val Context.myDataStoreOjbect: DataStore<Preferences> by preferencesDataStore("UserDataFile")
+        val USER_EMAIL_KEY=stringPreferencesKey("user_email")  //keyname to retrieve data
     }
+    //get email value from datastore
+
+    val getEmail: Flow<String?>=context.myDataStoreObject.data
+        .map { preferences->
+
+            preferences[USER_EMAIL_KEY]?:"FIRSTLAST@GMAIL.COM"
+    }
+    //save email
+
+    suspend fun saveEmail(name:String)
+    {
+        context.myDataStoreObject.edit { preferences->
+
+            preferences[USER_EMAIL_KEY]=name
+        }
+    }
+
 }
